ecdb6e5a3bf467b12afe6948bec7cfb5d675ee13
LayneHansen <layne.d.hansen@gmail.com>
2021-02-28 20:25:49 -0700

    added constructor, getters and setters

--- tech-news-java-api/src/main/java/com/technews/model/User.java
+++ tech-news-java-api/src/main/java/com/technews/model/User.java
@@ -9,6 +9,81 @@ import javax.persistence.*;
 @Table(name = "user")
 
 public class User {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public List<Vote> getVotes() {
+        return votes;
+    }
+
+    public void setVotes(List<Vote> votes) {
+        this.votes = votes;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public User(Integer id, String username, String email, String password, boolean loggedIn, List<Post> posts, List<Vote> votes, List<Comment> comments) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.loggedIn = loggedIn;
+        this.posts = posts;
+        this.votes = votes;
+        this.comments = comments;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
