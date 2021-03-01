89f4e0d2783fcbe333fd09c9a210f57f7577f45c
LayneHansen <layne.d.hansen@gmail.com>
2021-02-28 20:11:35 -0700

    added instance variables: id, username, email, password, loggedIn, posts, votes, comments

--- tech-news-java-api/src/main/java/com/technews/model/User.java
+++ tech-news-java-api/src/main/java/com/technews/model/User.java
@@ -9,7 +9,17 @@ import javax.persistence.Table;
 @Table(name = "user")
 
 public class User {
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+    boolean loggedIn;
 
+    private List<Post> posts;
+    private List<Vote> votes;
+    private List<Comment> comments;
 }
+
+
 
 
